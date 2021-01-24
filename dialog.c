aa071e32b7c0007266429fde3a8c6f00fb584c89
Quinn <lolwutfordawin@gmail.com>
2021-01-24 16:14:23 +0100

    Dialog Methode erweitert

--- dialog.c
+++ dialog.c
@@ -51,20 +51,40 @@ int param_dialog(param_t p, complex_t c)
 {
     param_t p_in = p;
     complex_t c_in =c;
+    int input =0;
+    int exit = 0;
     
-    printf("--------------------------------------------------------------------------\n");
-    printf("Zur Parametereingabe bitte eine der Folgenden Optionen auswählen: \n\n");
-    printf("0: Radius zur Beschreibung es kreisförmigen Gebietes G für komplexe Zahlen \n Aktuell: radius = %f", p_in.radius);
-    printf("\n1: Maximale Iterationszahl \n Aktuell: imax = %d", p_in.imax);
-    printf("\n2: Fraktalvariante wählen: 0 für Manedelbrot, 1 für Juliamenge \n Aktuell: menge = % d", p_in.menge);
-    printf("\n3: Komplexwertiges Analysegebiet wählen \n Aktuell: xmin = %f, xmax = %f, ymin = %f, ymax = %f", p_in.xmin, p_in.xmax, p_in.ymin, p_in.ymax);
-    printf("\n4: Anzahl der Linien im Analysegebiet \n Aktuell: Linien in x-Richtung: xpoints = %d \n          Linien in y-Richtung: ypoints = %d", p_in.xpoints, p_in.ypoints);
-    printf("\n5: Komplexe Zahl c\n Aktuell: Realteil = %f, Imaginärteil = %f", c_in.real, c_in.imag);
-    printf("\n--------------------------------------------------------------------------\n");
+    
+    while (exit == 0)
+    {
+        printf("--------------------------------------------------------------------------\n");
+        printf("Zur Parametereingabe bitte eine der Folgenden Optionen auswählen und mit [Enter] bestätigen: \n\n");
+        printf("1: Radius zur Beschreibung es kreisförmigen Gebietes G für komplexe Zahlen \n Aktuell: radius = %f", p_in.radius);
+        printf("\n2: Maximale Iterationszahl \n Aktuell: imax = %d", p_in.imax);
+        printf("\n3: Fraktalvariante wählen: 0 für Manedelbrot, 1 für Juliamenge \n Aktuell: menge = % d", p_in.menge);
+        printf("\n4: Komplexwertiges Analysegebiet wählen \n Aktuell: xmin = %f, xmax = %f, ymin = %f, ymax = %f", p_in.xmin, p_in.xmax, p_in.ymin, p_in.ymax);
+        printf("\n5: Anzahl der Linien im Analysegebiet \n Aktuell: Linien in x-Richtung: xpoints = %d \n          Linien in y-Richtung: ypoints = %d", p_in.xpoints, p_in.ypoints);
+        printf("\n6: Komplexe Zahl c\n Aktuell: Realteil = %f, Imaginärteil = %f", c_in.real, c_in.imag);
+        printf("\n\n9: Eingaben bestätigen und Speichern");
+        printf("\n0: Programm beenden");
+        printf("\n--------------------------------------------------------------------------\n");
 
+        input_int(&input);
 
+        switch (input)
+        {
+         
 
-    return 0;
+        case 1: printf("\nDie jew\n");
+            break;
+        default: printf("\n*******\nEingabe nicht erkannt: Bitte Enstrechende Zahl von 0 ... 9 eingeben!\n");
+            break;
+        }
+
+        return 0;
+    }
+
+    
 
 };
 

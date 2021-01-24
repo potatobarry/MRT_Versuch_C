53e56f4a890db9e296a5fe05961c37bcfb895c4f
Quinn <lolwutfordawin@gmail.com>
2021-01-24 14:23:43 +0100

    Dialog modifiziert

--- dialog.c
+++ dialog.c
@@ -51,20 +51,16 @@ int param_dialog(param_t p, complex_t c)
 {
     param_t p_in = p;
     complex_t c_in =c;
-    double x = 2.4;
-
-    printf("-------------------------------------------------------------------------- \n");
-    printf("Zur Parametereingabe bitte eine der Folgenden Optionen auswählen: \n\n");
-    printf("0: Radius zur Beschreibung es kreisförmigen Gebietes G für komplexe Zahlen \n Aktuell: p_radius = %f", p.radius);
-    printf("\n1: Maximale Iterationszahl \n Aktuell: imax = %d", p.imax);
-    printf("\n2: Fraktalvariante wählen: 0 für Manedelbrot, 1 für Juliamenge \n Aktuell: menge = % f", p.menge);
-    printf("\n3: Komplexwertiges Analysegebiet wählen \n Aktuell: p_radius = % f", p.radius);
-    printf("\n4: Anzahl der Linien im Analysegebiet \n Aktuell: p_radius = % f", p.radius);
-    printf("\n5: Komplexe \n");
     
-    printf( "\n");
-    printf(" \n");
-    printf(" \n");
+    printf("--------------------------------------------------------------------------\n");
+    printf("Zur Parametereingabe bitte eine der Folgenden Optionen auswählen: \n\n");
+    printf("0: Radius zur Beschreibung es kreisförmigen Gebietes G für komplexe Zahlen \n Aktuell: radius = %f", p_in.radius);
+    printf("\n1: Maximale Iterationszahl \n Aktuell: imax = %d", p_in.imax);
+    printf("\n2: Fraktalvariante wählen: 0 für Manedelbrot, 1 für Juliamenge \n Aktuell: menge = % d", p_in.menge);
+    printf("\n3: Komplexwertiges Analysegebiet wählen \n Aktuell: xmin = %f, xmax = %f, ymin = %f, ymax = %f", p_in.xmin, p_in.xmax, p_in.ymin, p_in.ymax);
+    printf("\n4: Anzahl der Linien im Analysegebiet \n Aktuell: Linien in x-Richtung: xpoints = %d \n          Linien in y-Richtung: ypoints = %d", p_in.xpoints, p_in.ypoints);
+    printf("\n5: Komplexe Zahl c\n Aktuell: Realteil = %f, Imaginärteil = %f", c_in.real, c_in.imag);
+    printf("\n--------------------------------------------------------------------------")
 
 
 
